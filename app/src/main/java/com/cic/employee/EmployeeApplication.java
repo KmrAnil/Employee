b9f84eabe1b63b1ef6b81370ab130c89948feafc
Anil Kumar <48729939+KmrAnil@users.noreply.github.com>
2023-08-04 11:06:19 +0530

    Project Refactoring

--- app/src/main/java/com/cic/employee/EmployeeApplication.java
+++ app/src/main/java/com/cic/employee/EmployeeApplication.java
@@ -2,9 +2,10 @@ package com.cic.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.cic.employee")
+@ComponentScan("com.cic.employee")
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
--- impl/src/main/java/com/cic/employee/entity/EmployeeEntity.java
+++ impl/src/main/java/com/cic/employee/entity/EmployeeEntity.java
@@ -1,11 +1,17 @@
 package com.cic.employee.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "employee")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
